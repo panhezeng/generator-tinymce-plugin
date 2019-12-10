ebaf2c7544d2213a7adf6732d837673ef176a1e6
Pan Hezeng <panhezeng@gmail.com>
2019-12-10 17:33:31 +0800

    update

--- generators/app/index.js
+++ generators/app/index.js
@@ -4,33 +4,11 @@ const chalk = require("chalk");
 const yosay = require("yosay");
 
 module.exports = class extends Generator {
-  prompting() {
-    // Have Yeoman greet the user.
-    this.log(
-      yosay(
-        `Welcome to the peachy ${chalk.red(
-          "generator-tinymce-plugin"
-        )} generator!`
-      )
-    );
-
-    const prompts = [
-      {
-        type: "confirm",
-        name: "someAnswer",
-        message: "Would you like to enable this option?",
-        default: true
-      }
-    ];
-
-    return this.prompt(prompts).then(props => {
-      // To access props later use this.props.someAnswer;
-      this.props = props;
-    });
-  }
-
   writing() {
-    this.fs.copy(this.templatePath(".gitignore"), this.destinationPath(".gitignore"));
+    this.fs.copy(
+      this.templatePath(".gitignore"),
+      this.destinationPath(".gitignore")
+    );
   }
 
   install() {
